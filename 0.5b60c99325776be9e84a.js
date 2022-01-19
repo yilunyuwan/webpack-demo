730035d3897426b336064e8bb428e001e6d927e6
yilunyuwna <nicolezrr@outlook.com>
2022-01-19 13:51:43 +0800

    remove source code

---
+++ 0.5b60c99325776be9e84a.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/lazy.js":
+/*!*********************!*\
+  !*** ./src/lazy.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function(){\r\n    console.log('我是懒加载')\r\n});\n\n//# sourceURL=webpack:///./src/lazy.js?");
+
+/***/ })
+
+}]);
